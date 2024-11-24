2357dbb5ada1979516981ddcb27b9642eaf9904f
Hardip Patel <hardip@anormaly.com>
2024-11-24 12:36:10 +0530

    feature/add-cloudflare: add Cloudflare Next.js integration and scripts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import {setupDevPlatform} from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
     images: {
@@ -7,4 +8,7 @@ const nextConfig: NextConfig = {
     },
 };
 
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+}
 export default nextConfig;
