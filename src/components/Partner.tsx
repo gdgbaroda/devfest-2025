14e1d58fe6d706f6c2891184859e3de0aa8938bd
Ayush Makwana <ayush.makwana700@gmail.com>
2025-09-14 20:56:05 +0530

    feat(ui): add JetBrains logo and update Partner component to include it

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -1,17 +1,17 @@
 import PartnerFrame from "@/components/PartnerFrame";
-import Callout from "./Callout";
 
 export default function Partner() {
   return (
     <div className="flex flex-col h-full w-full px-4 md:px-8 lg:px-16 xl:px-32 my-10 md:my-16 gap-6 md:gap-10 items-center text-center">
       <h1 className=" text-3xl lg:text-4xl font-bold">Community Partners</h1>
-      <Callout title="To be announced..." />
-      {/* <p className=" text-base md:text-lg lg:text-xl text-gray-700 max-w-4xl">
+      <p className=" text-base md:text-lg lg:text-xl text-gray-700 max-w-4xl">
         Our community partners play a vital role in supporting and enhancing our
         event. We are thankful for their collaboration and commitment.
-      </p> */}
-      {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 place-items-center">
-        <PartnerFrame name={"Appmattic"} logo={"/logo/Appmattic.svg"} />
+      </p>
+      {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 place-items-center"> */}
+      <div className="flex justify-center w-full">
+        <PartnerFrame name={"JetBrains"} logo={"/logo/jetbrains.svg"} />
+        {/* <PartnerFrame name={"Appmattic"} logo={"/logo/Appmattic.svg"} />
         <PartnerFrame name={"Atliq"} logo={"/logo/Atliq.svg"} />
         <PartnerFrame name={"Atyantik"} logo={"/logo/Atyantik.svg"} />
         <PartnerFrame name={"AV Devs"} logo={"/logo/AVDevs.svg"} />
@@ -21,8 +21,8 @@ export default function Partner() {
         <PartnerFrame name={"Pardy Panda"} logo={"/logo/pardy-panda.svg"} />
         <PartnerFrame name={"Pavans"} logo={"/logo/Pavans.svg"} />
         <PartnerFrame name={"SunflowerLabs"} logo={"/logo/SunflowerLabs.svg"} />
-        <PartnerFrame name={"Anormaly"} logo={"/logo/Anormaly.svg"} />
-      </div> */}
+        <PartnerFrame name={"Anormaly"} logo={"/logo/Anormaly.svg"} /> */}
+      </div>
     </div>
   );
 }
