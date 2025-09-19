c6828051a611865e0fd8458769c5d85c313dee4c
knightkill <hardipdpatel@gmail.com>
2025-09-19 12:36:22 +0530

    feat(lib): add attendee data interface and sample list
    - What changed:
      • Created AttendeeData interface defining attendee properties.
      • Added ATTENDEES_DATA array with sample attendee objects.
    - Why:
      • Provide a structured data model for attendees to support event features.
      • Seed initial attendee data for testing and development purposes.
    - Affected files/topics:
      • src/lib/attendees.ts

---
+++ src/lib/attendees.ts
@@ -0,0 +1,48 @@
+export interface AttendeeData {
+  name: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  ticketType: string;
+  ticketNo: string;
+  qrCode: string;
+}
+
+export const ATTENDEES_DATA: AttendeeData[] = [
+  {
+    name: 'Aditipriya Dubey',
+    email: 'aditipriyamdubey@gmail.com',
+    firstName: 'Aditipriya',
+    lastName: 'Dubey',
+    ticketType: 'Workshop Day - Oct 4 2025 - General Entry',
+    ticketNo: '362',
+    qrCode: 'HL611059HC'
+  },
+  {
+    name: 'Jiya Singh',
+    email: 'jiy5239@gmail.com',
+    firstName: 'Jiya',
+    lastName: 'Singh',
+    ticketType: 'Workshop Day - Oct 4 2025 - General Entry',
+    ticketNo: '363',
+    qrCode: 'FC405182HR'
+  },
+  {
+    name: 'Shagun Chaudhari',
+    email: 'shagun22r@gmail.com',
+    firstName: 'Shagun',
+    lastName: 'Chaudhari',
+    ticketType: 'Workshop Day - Oct 4 2025 - General Entry',
+    ticketNo: '364',
+    qrCode: 'DS580716LK'
+  },
+  {
+    name: 'Rohini Pandya',
+    email: 'rohinip711@gmail.com',
+    firstName: 'Rohini',
+    lastName: 'Pandya',
+    ticketType: 'Workshop Day - Oct 4 2025 - General Entry',
+    ticketNo: '365',
+    qrCode: 'AO545195XE'
+  }
+];
